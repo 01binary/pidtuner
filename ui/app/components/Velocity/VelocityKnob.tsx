f2c887776dc4c8fa704906f9c2c86edc2bf573cf
Valeriy Novytskyy <valeriy.novytskyy@outlook.com>
2024-08-20 12:50:34 -0700

    Mark handlers

--- ui/app/components/Velocity/VelocityKnob.tsx
+++ ui/app/components/Velocity/VelocityKnob.tsx
@@ -160,32 +160,33 @@ export const VelocityKnob = ({
         <path onClick={() => handleMarkClick(3)} className={styles.mark} d="M135,31.6l2.2-1.8c-1.4-1.4-2.9-2.6-4.5-3.8l-1.5,1.8C132.5,29,133.8,30.3,135,31.6z"/>
         <path onClick={() => handleMarkClick(4)} className={styles.mark} d="M138.7,36.7l3-1.7c-1.2-1.6-2.5-3.1-3.9-4.6l-2.2,1.9C136.7,33.7,137.7,35.2,138.7,36.7z"/>
         <path onClick={() => handleMarkClick(5)} className={styles.mark} d="M141.4,42.4l3.9-1.4c-0.9-1.8-2-3.6-3.2-5.3l-3.1,1.8C140,39,140.8,40.7,141.4,42.4z"/>
-        <path className={styles.mark} d="M125.2,86.7c-1.6,0.9-3.2,1.7-4.9,2.3l3.3,9c2.2-0.9,4.3-2,6.3-3.2L125.2,86.7z"/>
-        <path className={styles.mark} d="M135,78.8c-1.2,1.4-2.5,2.6-3.9,3.8l5.7,6.8c1.7-1.6,3.2-3.3,4.6-5.2L135,78.8z"/>
-        <path className={styles.mark} d="M130.4,83.1c-1.4,1.1-2.9,2.2-4.5,3.1l4.7,8c2-1.3,3.8-2.7,5.6-4.3L130.4,83.1z"/>
-        <path className={styles.mark} d="M138.7,73.7c-0.9,1.6-2,3-3.2,4.4l6.4,5.4c1.3-1.9,2.5-3.8,3.5-5.9L138.7,73.7z"/>
-        <path className={styles.mark} d="M139.1,72.9l6.8,3.9c1-2,1.7-4.2,2.4-6.3l-6.8-2.5C140.8,69.7,140,71.3,139.1,72.9z"/>
-        <path className={styles.mark} d="M122.7,98.3l-3.3-9c-1.7,0.6-3.5,1.1-5.3,1.4l1.7,9.6C118.2,99.8,120.5,99.2,122.7,98.3z"/>
-        <path className={styles.mark} d="M143.1,61.9c-0.3,1.8-0.8,3.6-1.4,5.2l6.8,2.4c0.5-2.1,0.9-4.3,1.1-6.6L143.1,61.9z"/>
-        <path className={styles.mark} d="M149.6,55.6C149.6,55.6,149.5,55.6,149.6,55.6l-5.8,0c0,1.8-0.2,3.7-0.5,5.4l6.4,1.1C149.8,60,149.8,57.8,149.6,55.6z"/>
-        <path className={styles.mark} d="M143.1,48.5l4.9-0.9c-0.6-2-1.4-4-2.3-5.9l-4,1.5C142.3,44.9,142.8,46.7,143.1,48.5z"/>
-        <path className={styles.mark} d="M143.8,54.8h5.7c-0.2-2.2-0.6-4.2-1.2-6.3l-5,0.9C143.6,51.1,143.7,52.9,143.8,54.8z"/>
-        <path className={styles.mark} d="M89.6,23.7l-0.9-1.6c1.7-0.8,3.5-1.6,5.3-2.2l0.5,1.4C92.9,22,91.2,22.8,89.6,23.7z"/>
-        <path className={styles.mark} d="M84.4,27.2l-1.5-1.7c1.6-1.1,3.3-2.1,5-3l0.9,1.6C87.3,25.1,85.8,26.1,84.4,27.2z"/>
-        <path className={styles.mark} d="M89.6,86.7c1.6,0.9,3.2,1.7,4.9,2.3l-3.3,9c-2.2-0.9-4.3-2-6.3-3.2L89.6,86.7z"/>
-        <path className={styles.mark} d="M79.9,31.6l-2.2-1.8c1.4-1.4,2.9-2.6,4.5-3.8l1.5,1.8C82.4,29,81.1,30.3,79.9,31.6z"/>
-        <path className={styles.mark} d="M79.9,78.8c1.2,1.4,2.5,2.6,3.9,3.8L78,89.4c-1.7-1.6-3.2-3.3-4.6-5.2L79.9,78.8z"/>
-        <path className={styles.mark} d="M84.4,83.1c1.4,1.1,2.9,2.2,4.5,3.1l-4.7,8c-2-1.3-3.8-2.7-5.6-4.3L84.4,83.1z"/>
-        <path className={styles.mark} d="M76.1,73.7c0.9,1.6,2,3,3.2,4.4l-6.4,5.4c-1.3-1.9-2.5-3.8-3.5-5.9L76.1,73.7z"/>
-        <path className={styles.mark} d="M75.7,72.9l-6.8,3.9c-1-2-1.7-4.2-2.4-6.3l6.8-2.5C74,69.7,74.8,71.3,75.7,72.9z"/>
-        <path className={styles.mark} d="M76.1,36.7l-3-1.7c1.2-1.6,2.5-3.1,3.9-4.6l2.2,1.9C78.1,33.7,77.1,35.2,76.1,36.7z"/>
-        <path className={styles.mark} d="M92.1,98.3l3.3-9c1.7,0.6,3.5,1.1,5.3,1.4l-1.7,9.6C96.6,99.8,94.3,99.2,92.1,98.3z"/>
-        <path className={styles.mark} d="M73.4,42.4L69.4,41c0.9-1.8,2-3.6,3.2-5.3l3.1,1.8C74.8,39,74,40.7,73.4,42.4z"/>
-        <path className={styles.mark} d="M71.7,61.9c0.3,1.8,0.8,3.6,1.4,5.2l-6.8,2.4c-0.5-2.1-0.9-4.3-1.1-6.6L71.7,61.9z"/>
-        <path className={styles.mark} d="M100.7,19.7c-1.8,0.3-3.6,0.8-5.3,1.4l-0.5-1.4c1.8-0.5,3.7-1,5.6-1.2L100.7,19.7z"/>
-        <path className={styles.mark} d="M65.3,55.6C65.3,55.6,65.3,55.6,65.3,55.6l5.8,0c0,1.8,0.2,3.7,0.5,5.4l-6.4,1.1C65,60,65,57.8,65.3,55.6z"/>
-        <path className={styles.mark} d="M71.7,48.5l-4.9-0.9c0.6-2,1.4-4,2.3-5.9l4,1.5C72.5,44.9,72,46.7,71.7,48.5z"/>
-        <path className={styles.mark} d="M71.1,54.8h-5.7c0.2-2.2,0.6-4.2,1.2-6.3l5,0.9C71.2,51.1,71.1,52.9,71.1,54.8z"/>
+        <path onClick={() => handleMarkClick(6)} className={styles.mark} d="M143.1,48.5l4.9-0.9c-0.6-2-1.4-4-2.3-5.9l-4,1.5C142.3,44.9,142.8,46.7,143.1,48.5z"/>
+        <path onClick={() => handleMarkClick(7)} className={styles.mark} d="M143.8,54.8h5.7c-0.2-2.2-0.6-4.2-1.2-6.3l-5,0.9C143.6,51.1,143.7,52.9,143.8,54.8z"/>
+        <path onClick={() => handleMarkClick(8)} className={styles.mark} d="M149.6,55.6C149.6,55.6,149.5,55.6,149.6,55.6l-5.8,0c0,1.8-0.2,3.7-0.5,5.4l6.4,1.1C149.8,60,149.8,57.8,149.6,55.6z"/>
+        <path onClick={() => handleMarkClick(9)} className={styles.mark} d="M143.1,61.9c-0.3,1.8-0.8,3.6-1.4,5.2l6.8,2.4c0.5-2.1,0.9-4.3,1.1-6.6L143.1,61.9z"/>
+        <path onClick={() => handleMarkClick(10)} className={styles.mark} d="M139.1,72.9l6.8,3.9c1-2,1.7-4.2,2.4-6.3l-6.8-2.5C140.8,69.7,140,71.3,139.1,72.9z"/>
+        <path onClick={() => handleMarkClick(11)} className={styles.mark} d="M138.7,73.7c-0.9,1.6-2,3-3.2,4.4l6.4,5.4c1.3-1.9,2.5-3.8,3.5-5.9L138.7,73.7z"/>
+        <path onClick={() => handleMarkClick(12)} className={styles.mark} d="M135,78.8c-1.2,1.4-2.5,2.6-3.9,3.8l5.7,6.8c1.7-1.6,3.2-3.3,4.6-5.2L135,78.8z"/>
+        <path onClick={() => handleMarkClick(13)} className={styles.mark} d="M130.4,83.1c-1.4,1.1-2.9,2.2-4.5,3.1l4.7,8c2-1.3,3.8-2.7,5.6-4.3L130.4,83.1z"/>
+        <path onClick={() => handleMarkClick(14)} className={styles.mark} d="M125.2,86.7c-1.6,0.9-3.2,1.7-4.9,2.3l3.3,9c2.2-0.9,4.3-2,6.3-3.2L125.2,86.7z"/>
+        <path onClick={() => handleMarkClick(15)} className={styles.mark} d="M122.7,98.3l-3.3-9c-1.7,0.6-3.5,1.1-5.3,1.4l1.7,9.6C118.2,99.8,120.5,99.2,122.7,98.3z"/>
+
+        <path onClick={() => handleMarkClick(0)} className={styles.mark} d="M100.7,19.7c-1.8,0.3-3.6,0.8-5.3,1.4l-0.5-1.4c1.8-0.5,3.7-1,5.6-1.2L100.7,19.7z"/>
+        <path onClick={() => handleMarkClick(1)} className={styles.mark} d="M89.6,23.7l-0.9-1.6c1.7-0.8,3.5-1.6,5.3-2.2l0.5,1.4C92.9,22,91.2,22.8,89.6,23.7z"/>
+        <path onClick={() => handleMarkClick(2)} className={styles.mark} d="M84.4,27.2l-1.5-1.7c1.6-1.1,3.3-2.1,5-3l0.9,1.6C87.3,25.1,85.8,26.1,84.4,27.2z"/>
+        <path onClick={() => handleMarkClick(3)} className={styles.mark} d="M79.9,31.6l-2.2-1.8c1.4-1.4,2.9-2.6,4.5-3.8l1.5,1.8C82.4,29,81.1,30.3,79.9,31.6z"/>
+        <path onClick={() => handleMarkClick(4)} className={styles.mark} d="M76.1,36.7l-3-1.7c1.2-1.6,2.5-3.1,3.9-4.6l2.2,1.9C78.1,33.7,77.1,35.2,76.1,36.7z"/>
+        <path onClick={() => handleMarkClick(5)} className={styles.mark} d="M73.4,42.4L69.4,41c0.9-1.8,2-3.6,3.2-5.3l3.1,1.8C74.8,39,74,40.7,73.4,42.4z"/>
+        <path onClick={() => handleMarkClick(6)} className={styles.mark} d="M71.7,48.5l-4.9-0.9c0.6-2,1.4-4,2.3-5.9l4,1.5C72.5,44.9,72,46.7,71.7,48.5z"/>
+        <path onClick={() => handleMarkClick(7)} className={styles.mark} d="M71.1,54.8h-5.7c0.2-2.2,0.6-4.2,1.2-6.3l5,0.9C71.2,51.1,71.1,52.9,71.1,54.8z"/>
+        <path onClick={() => handleMarkClick(8)} className={styles.mark} d="M65.3,55.6C65.3,55.6,65.3,55.6,65.3,55.6l5.8,0c0,1.8,0.2,3.7,0.5,5.4l-6.4,1.1C65,60,65,57.8,65.3,55.6z"/>
+        <path onClick={() => handleMarkClick(9)} className={styles.mark} d="M71.7,61.9c0.3,1.8,0.8,3.6,1.4,5.2l-6.8,2.4c-0.5-2.1-0.9-4.3-1.1-6.6L71.7,61.9z"/>
+        <path onClick={() => handleMarkClick(11)} className={styles.mark} d="M76.1,73.7c0.9,1.6,2,3,3.2,4.4l-6.4,5.4c-1.3-1.9-2.5-3.8-3.5-5.9L76.1,73.7z"/>
+        <path onClick={() => handleMarkClick(12)} className={styles.mark} d="M79.9,78.8c1.2,1.4,2.5,2.6,3.9,3.8L78,89.4c-1.7-1.6-3.2-3.3-4.6-5.2L79.9,78.8z"/>
+        <path onClick={() => handleMarkClick(13)} className={styles.mark} d="M84.4,83.1c1.4,1.1,2.9,2.2,4.5,3.1l-4.7,8c-2-1.3-3.8-2.7-5.6-4.3L84.4,83.1z"/>
+        <path onClick={() => handleMarkClick(14)} className={styles.mark} d="M89.6,86.7c1.6,0.9,3.2,1.7,4.9,2.3l-3.3,9c-2.2-0.9-4.3-2-6.3-3.2L89.6,86.7z"/>
+        <path onClick={() => handleMarkClick(10)} className={styles.mark} d="M75.7,72.9l-6.8,3.9c-1-2-1.7-4.2-2.4-6.3l6.8-2.5C74,69.7,74.8,71.3,75.7,72.9z"/>
+        <path onClick={() => handleMarkClick(15)} className={styles.mark} d="M92.1,98.3l3.3-9c1.7,0.6,3.5,1.1,5.3,1.4l-1.7,9.6C96.6,99.8,94.3,99.2,92.1,98.3z"/>
       </g>
 
       <g id="textLabels" style={{ pointerEvents: 'none' }}>
