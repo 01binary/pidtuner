9dfa0fc7e926acb91162acacb113acf86fd94684
Valeriy Novytskyy <valeriy.novytskyy@outlook.com>
2024-08-20 21:40:15 -0700

    Velocity change via number input

--- ui/app/components/Velocity/Velocity.tsx
+++ ui/app/components/Velocity/Velocity.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { PrimaryInput } from "../PrimaryInput";
 import { Separator } from "../Separator";
 import { VelocityKnob } from "./VelocityKnob";
@@ -8,28 +8,32 @@ import { VelocityKnob } from "./VelocityKnob";
 export const Velocity = () => {
   const [velocity, setVelocity] = useState(0);
 
-  return (
-  <section className="module">
-    <section className="module__header">
-      <img src="/velocity.svg" width="32" height="32" />
-      <h2>Velocity</h2>
-    </section>
+  const handleChangeVelocity = useCallback((e) => {
+    setVelocity(e.target.value / 100);
+  }, []);
 
-    <section className="module__controls">
-      <PrimaryInput
-        type="number"
-        value={Math.round(velocity * 100)}
-        onChange={(e) => setVelocity(e.target.value)}
-        min="-100"
-        max="100"
-        step="1"
-      />
-     
-      <VelocityKnob
-        velocity={velocity}
-        handleChange={setVelocity}
-      />
+  return (
+    <section className="module">
+      <section className="module__header">
+        <img src="/velocity.svg" width="32" height="32" />
+        <h2>Velocity</h2>
+      </section>
+
+      <section className="module__controls">
+        <PrimaryInput
+          type="number"
+          value={Math.round(velocity * 100)}
+          onChange={handleChangeVelocity}
+          min="-100"
+          max="100"
+          step="1"
+        />
+      
+        <VelocityKnob
+          velocity={velocity}
+          handleChange={setVelocity}
+        />
+      </section>
     </section>
-  </section>
-);
+  );
 }
